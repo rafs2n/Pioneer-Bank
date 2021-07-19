377cb8850f93102c98e4b917d7f964e2a8f982e9
Abdul Aziz <md.aziz@northsouth.edu>
2021-07-20 00:22:13 +0600

    dashboard added

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+const trxArea = document.getElementById('transaction-area');
+trxArea.style.display = 'block'
+
 const btnClick = document.getElementById('login');
 btnClick.addEventListener('click', function () {
-    alert('I am Clicked')
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+    trxArea.style.display = 'block';
+})
+
+// Deposit event handler
+
+const depositBtn = document.getElementById('deposit');
+depositBtn.addEventListener('click', function () {
+    const depositAmount = parseFloat(document.getElementById('deposit-amount').value);
+    document.getElementById('deposit-amount').value = "";
+    const currentDeposit = parseFloat(document.getElementById('current-deposit').innerText);
+    const totalDeposit = depositAmount + currentDeposit;
+    document.getElementById('current-deposit').innerText = totalDeposit;
+
 })
